1af6907701daee49d46a0b3a2867788dbaf284c7
Adele <Adele@BIGSUR>
2016-05-03 12:20:42 +0100

    First commit

---
+++ ProjectEuler/ProjectEuler/PE_Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectEulerResults;
+
+namespace ProjectEuler
+{
+    [TestClass]
+    public class PE_Test
+    {
+        [TestMethod]
+        public void RemovedSumFromInitialAmount()
+        {
+            int value = 10;
+            int divisor = 3;
+            int result = 18;
+
+            PE_Task1 T1 = new PE_Task1(value, divisor);
+            T1.GenerateSumOfMultiples();
+
+            // assert
+            double actual = T1.SumOfMultiples;
+            Assert.AreEqual(result, actual, "Incorrect number of multiples!");
+        }
+    }
+}
---
+++ ProjectEuler/ProjectEulerResults/PE_Task1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEulerResults
+{
+    //If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+    //The sum of these multiples is 23.
+    //Find the sum of all the multiples of 3 or 5 below 1000.
+
+    public class PE_Task1
+    {
+        private int p_MaxValue;
+        private int p_Divisor;
+        private int p_SumOfMultiples;
+
+        public static void Main()
+        {
+            //PE_Task1 T1 = new PE_Task1(10, 3);
+            //T1.GetSumOfMultiples();
+            //Console.WriteLine(T1.MultipleSum);
+        }
+
+        public PE_Task1(int maxValue, int divisor)
+        {
+            p_MaxValue = maxValue;
+            p_Divisor = divisor;
+        }
+        
+        public void GenerateSumOfMultiples()
+        {
+            for (int i = 0; i <= p_MaxValue; i++)
+            {
+                if (i % p_Divisor == 0)
+                {
+                    p_SumOfMultiples += i;
+                }
+            }
+        }
+
+        public int SumOfMultiples
+        {
+            get { return p_SumOfMultiples; }
+        }
+
+
+        //private int p_MaxValue;
+        //private double p_Multiples;
+
+        //public PE_Task1(int maxValue)
+        //{
+        //    p_MaxValue = maxValue;
+        //}
+
+        //public double Multiples
+        //{
+        //    get { return p_Multiples; }
+        //}
+
+        //public void GetMultiplesOf(int multiple)
+        //{
+        //    p_Multiples = p_MaxValue / multiple;
+        //}
+
+        //public static void Main()
+        //{
+        //    //PE_Task1 T1 = new PE_Task1(1000);
+        //    //T1.GetMultiplesOf(5);
+        //}
+    }
+}
